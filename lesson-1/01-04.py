0960b15175811a56bfd752a5d991e02d04ab4dee
shmonoff <shmonoff@mail.ru>
2017-11-01 14:11:21 +1000

    Update and rename 01.04.py to 01-04.py

--- lesson-1/01-04.py
+++ lesson-1/01-04.py
@@ -1,19 +1,16 @@
 import math
 
-def getPoint(v,t):
 
+def getPoint(v,t):
+  
+  roadLength = 109
   reverse = False
 
   if (v<0): reverse = True
 
-  roadLength = 109
-
   v = math.fabs(v)
-
   fullPath = v*t
-
   path = fullPath % roadLength
-
   diff = fullPath - path
 
   if (diff > 0):
