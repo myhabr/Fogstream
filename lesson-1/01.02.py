e77f491421b437d393d59763b0056635d886fc0c
shmonoff <shmonoff@mail.ru>
2017-11-01 10:46:06 +1000

    Update 01.02.py

--- lesson-1/01.02.py
+++ lesson-1/01.02.py
@@ -1,18 +1,18 @@
 import math
-import numpy as np
+
 
 def countTables(a,b,c):
-  
-  students = np.array([int(a),int(b),int(c)])
-  
-  def itemTables(x):
+
+  def prepareData(x):
+    return int(math.fabs(x))
     
+  def itemTables(x):
     return math.ceil(x/2)
     
-  itemTables = np.vectorize(itemTables, otypes=[np.int])
+  students = list(map(prepareData, [a,b,c]))
+    
+  tables = sum(list(map(itemTables, students)))
 
-  tables = np.sum(itemTables(students))
+  print (tables)
 
-  print ( tables )
-  
-countTables(2,3,15)
+countTables(2.3,3,-4)
