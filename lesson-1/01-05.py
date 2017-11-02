9571e81043988d50cd6d254c9ea7c772bd2259da
shmonoff <shmonoff@mail.ru>
2017-11-02 22:45:49 +1000

    Update 01-05.py

--- lesson-1/01-05.py
+++ lesson-1/01-05.py
@@ -1,54 +1,37 @@
-def countTime():
-  
-  print("введите кол-во уроков:")
+def calcTime():
 
-  n = int(input());
+  n = int(input("введите кол-во уроков:"));
 
   if (n<1): n = 1
   if (n>10): n = 10
   
-  start_h = 9
-  start_m = 0
+  bgn = [9,00]
+  lesDur = 45
+  brOdd = 5
+  brEv = 15
   
-  les_dur = 45
-  br_odd = 5
-  br_ev = 15
-  
-  n_br = n-1
-  
-  n_br_ev = n_br_odd = 0
-  
-  if (n_br == 1): n_br_odd = 1
-  
-  if (n_br>1):
+  nBr = n-1
+  nBrEv = 0
 
-    if (n_br%2 == 0):
-      n_br_odd = n_br_ev = n_br/2
-  
-    else:
-      n_br_ev = (n_br-1)/2
-      n_br_odd = n_br_ev+1
+  if (nBr>1): nBrEv = nBr//2
 
-  com_dur = n*les_dur + n_br_ev*br_ev + n_br_odd*br_odd
+  nBrOdd = nBr - nBrEv
+  fullDur = n*lesDur + nBrEv*brEv + nBrOdd*brOdd
   
-  print("уроков: {}; перемен: {}, {}".format(n, int(n_br_odd), int(n_br_ev)))
+  print("уроков: {}; перемен: {}(5), {}(15)".format(n, int(nBrOdd), int(nBrEv)))
   
-  convertTime(com_dur, start_h, start_m)
+  convertTime(fullDur, bgn)
   
-def convertTime(n, h_st, m_st):
-  
-  n += h_st*60 + m_st
+def convertTime(n, start):
 
+  n += start[0]*60 + start[1]
   m = int(n % 60)
   diff = n - m
-    
-  if (diff > 0): h = int(diff / 60)
 
-  h = str(h); m = str(m)
-
-  if (len(h) == 1): h = "0" + h
-  if (len(m) == 1): m = "0" + m
+  if (diff): h = int(diff / 60)
+  if (h<10): h = "0" + str(h)
+  if (m<10): m = "0" + str(m)
 
   print ("уроки закончились в {}:{}".format(h,m))
 
-countTime()
+calcTime()
