58bf9025a9cfe741a9bc843560798e609db2be30
shmonoff <shmonoff@mail.ru>
2017-11-02 22:01:47 +1000

    Update 01-03.py

--- lesson-1/01-03.py
+++ lesson-1/01-03.py
@@ -1,36 +1,23 @@
-import math
-
-
-def minToTime(n):
+def minToTime():
   
-  n = int(math.fabs(n))
+  n = int(input("введите кол-во минут:"))
   h = 0; m = 0
   
-  if (n > 0):
+  if (n):
     
     diff = n - n % (60*24)
     
-    if (diff > 0): n = n % (60*24)
+    if (diff): n = n % (60*24)
     
     m = n % 60
     diff = n - m
     
-    if (diff > 0): h = int(diff / 60)
+    if (diff): h = int(diff / 60)
 
-  h = str(h); m = str(m)
+  if (h < 10): h = "0" + str(h)
+  if (m < 10): m = "0" + str(m)
 
-  if (len(h) == 1): h = "0" + h
-  if (len(m) == 1): m = "0" + m
+  print ("на часах {}:{}".format(h,m))
 
-  print ("{}:{}".format(h,m))
 
-minToTime(0)
-minToTime(59)
-minToTime(60)
-minToTime(-500)
-minToTime(1439)
-minToTime(1440)
-minToTime(1499)
-minToTime(778643)
-minToTime(345345.5464)
-minToTime(-77.8643)
+minToTime()
