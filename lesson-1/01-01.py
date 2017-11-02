4e77c6f1d9cfd172acdbf39dfe2d3fe937ddb7b8
shmonoff <shmonoff@mail.ru>
2017-11-02 21:20:49 +1000

    Update 01-01.py

--- lesson-1/01-01.py
+++ lesson-1/01-01.py
@@ -2,17 +2,16 @@ import math
 import cmath
 
 
-X = 1
-Y = -1
+def convertComplex():
 
-def convertComplex(x,y):
+  x, y = map(int, input("введите X Y:").split())
 
   z = math.sqrt(x*x + y*y) # math.hypot(x, y)
 
   _phi = 0
   m = [0,0]
 
-  if(z != 0):
+  if(z > 0):
 
     _phi = math.asin(math.fabs(y)/z)
     m = [0,1]
@@ -26,7 +25,7 @@ def convertComplex(x,y):
   phi = m[0]*math.pi + m[1]*_phi
   
   print("({}, {})".format(z, phi))
-  
-convertComplex(X,Y)
+  print(cmath.polar(complex(x,y)))
 
-print(cmath.polar(complex(X,Y)))
+
+convertComplex()
