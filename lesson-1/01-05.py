1526a7db8467ff6bdb5ec5efd08d42fddb0b0f11
shmonoff <shmonoff@mail.ru>
2017-11-03 22:27:05 +1000

    Update 01-05.py

--- lesson-1/01-05.py
+++ lesson-1/01-05.py
@@ -1,35 +1,35 @@
 def calcTime():
-
-  n = int(input("введите кол-во уроков:"));
-
-  if (n<1): n = 1
-  if (n>10): n = 10
   
   bgn = [9,00]
   lesDur = 45
   brOdd = 5
   brEv = 15
+
+  n = int(input("введите кол-во уроков:"))
+
+  if n<1: n = 1
+  if n>10: n = 10
   
   nBr = n-1
   nBrEv = 0
 
-  if (nBr>1): nBrEv = nBr//2
+  if nBr>1: nBrEv = nBr//2
 
   nBrOdd = nBr - nBrEv
   fullDur = n*lesDur + nBrEv*brEv + nBrOdd*brOdd
   
-  # print("уроков: {}; перемен: {}(5), {}(15)".format(n, int(nBrOdd), int(nBrEv)))
+  # print("уроков: {}; перемен: {}({}), {}({})".format(n, int(nBrOdd), brOdd, int(nBrEv), brEv))
   print("уроки заканчиваются в {}".format(convertTime(fullDur, bgn)))
   
-def convertTime(n, start):
+def convertTime(n, bgn):
 
-  n += start[0]*60 + start[1]
+  n += bgn[0]*60 + bgn[1]
   m = int(n % 60)
   diff = n - m
 
-  if (diff): h = int(diff / 60)
-  if (h<10): h = "0" + str(h)
-  if (m<10): m = "0" + str(m)
+  if diff: h = int(diff / 60)
+  if h<10: h = "0" + str(h)
+  if m<10: m = "0" + str(m)
 
   return ("{}:{}".format(h,m))
 
