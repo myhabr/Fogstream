77abf4c87379beb6f5301abc6ce14cdf153d70db
shmonoff <shmonoff@mail.ru>
2017-11-03 22:44:50 +1000

    Update 01-01.py

--- lesson-1/01-01.py
+++ lesson-1/01-01.py
@@ -4,26 +4,26 @@ import cmath
 
 def convertComplex():
 
-  x, y = map(int, input("введите X Y:").split())
+  x, y = map(float, input("введите X Y:").split())
 
   z = math.sqrt(x*x + y*y) # math.hypot(x, y)
 
   _phi = 0
   m = [0,0]
 
-  if(z > 0):
+  if z:
 
     _phi = math.asin(math.fabs(y)/z)
     m = [0,1]
 
-    if (x < 0):       m = [ 1, 0]
-    if (y < 0):       m = [ 0,-1]
-    if (x<0 and y>0): m = [ 1,-1]
-    if (x<0 and y<0): m = [-1, 1]
-    if (x>0 and y<0): m = [ 0,-1]
+    if x < 0:       m = [ 1, 0]
+    if y < 0:       m = [ 0,-1]
+    if x<0 and y>0: m = [ 1,-1]
+    if x<0 and y<0: m = [-1, 1]
+    if x>0 and y<0: m = [ 0,-1]
 
   phi = m[0]*math.pi + m[1]*_phi
-  
+
   print("({}, {})".format(z, phi))
   print(cmath.polar(complex(x,y)))
 
