11d73f672ebf5c9eb2f58e628a1e06dc8639e76b
shmonoff <shmonoff@mail.ru>
2017-11-03 22:39:39 +1000

    Update 01-03.py

--- lesson-1/01-03.py
+++ lesson-1/01-03.py
@@ -1,23 +1,24 @@
-def minToTime():
+def minToClock():
   
-  n = int(input("введите кол-во минут:"))
   h = 0; m = 0
   
-  if (n):
+  n = int(input("введите кол-во минут:"))
+  
+  if n:
     
     diff = n - n % (60*24)
     
-    if (diff): n = n % (60*24)
-    
+    if diff: n = n % (60*24)
+
     m = n % 60
     diff = n - m
     
-    if (diff): h = int(diff / 60)
+    if diff: h = int(diff/60)
 
-  if (h < 10): h = "0" + str(h)
-  if (m < 10): m = "0" + str(m)
+  if h < 10: h = "0" + str(h)
+  if m < 10: m = "0" + str(m)
 
   print ("на часах {}:{}".format(h,m))
 
 
-minToTime()
+minToClock()
