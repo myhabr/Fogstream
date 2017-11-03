c32b4770f03f094a7d180c59bff383972b2974e8
shmonoff <shmonoff@mail.ru>
2017-11-03 22:41:31 +1000

    Update 01-02.py

--- lesson-1/01-02.py
+++ lesson-1/01-02.py
@@ -1,7 +1,7 @@
 def countTables():
   
   def itemTables(x):
-    if (x%2): x+=1
+    if x%2: x+=1
     return x/2
   
   students = map(int, input("введите кол-во учеников 3-х классов через пробел:").split())
