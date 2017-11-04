962133c3142d5c60c8ebb5ad4a8a6d3025ede3b0
shmonoff <shmonoff@mail.ru>
2017-11-04 13:36:44 +1000

    Update 01-04.py

--- lesson-1/01-04.py
+++ lesson-1/01-04.py
@@ -1,23 +1,21 @@
-def getPoint():
-  
+def getPoint(n):
   roadLength = 109
   reverse = 0
-  
-  v,t = map(float, input("введите скорость (км/ч) и время движения (ч) через пробел:").split())
-  
+  v,t = map(float, n.split())
   if v<0:
     reverse = 1
     v = -v
-
   fullPath = v*t
   path = fullPath % roadLength
   diff = fullPath - path
-
   if diff: fullPath = path
   if reverse: fullPath = roadLength - fullPath
-
   fullPath = round(fullPath)
+  print("\n> остановка на отметке {}км\n".format(fullPath))
   
-  print("остановка на отметке {}км".format(fullPath))
-
-getPoint()
+while 1:
+  string = input("введите скорость (км/ч)\n и время движения (ч) через пробел или -s:")
+  if string.lower() == '-s':
+    print("\n> --- программа остановлена ---\n")
+    break
+  getPoint(string)
