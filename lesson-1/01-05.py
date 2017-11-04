4a3962218ddfe7d9700bf19cccab9e44f3e63e4c
shmonoff <shmonoff@mail.ru>
2017-11-04 13:47:16 +1000

    Update 01-05.py

--- lesson-1/01-05.py
+++ lesson-1/01-05.py
@@ -1,36 +1,29 @@
-def calcTime():
-  
-  bgn = [9,00]
-  lesDur = 45
-  brOdd = 5
-  brEv = 15
-
-  n = int(input("введите кол-во уроков:"))
-
-  if n<1: n = 1
-  if n>10: n = 10
-  
-  nBr = n-1
-  nBrEv = 0
-
-  if nBr>1: nBrEv = nBr//2
-
-  nBrOdd = nBr - nBrEv
-  fullDur = n*lesDur + nBrEv*brEv + nBrOdd*brOdd
-  
-  # print("уроков: {}; перемен: {}({}), {}({})".format(n, int(nBrOdd), brOdd, int(nBrEv), brEv))
-  print("уроки заканчиваются в {}".format(convertTime(fullDur, bgn)))
-  
 def convertTime(n, bgn):
-
   n += bgn[0]*60 + bgn[1]
   m = int(n % 60)
   diff = n - m
-
   if diff: h = int(diff / 60)
   if h<10: h = "0" + str(h)
   if m<10: m = "0" + str(m)
-
   return ("{}:{}".format(h,m))
 
-calcTime()
+def calcTime(n):
+  bgn = [9,00]
+  lesDur, brOdd, brEv = 45, 5, 15
+  n = int(n)
+  if n<1: n = 1
+  if n>10: n = 10
+  nBr = n-1
+  nBrEv = 0
+  if nBr>1: nBrEv = nBr//2
+  nBrOdd = nBr - nBrEv
+  fullDur = n*lesDur + nBrEv*brEv + nBrOdd*brOdd
+  print("\n> уроков: {}; перемен: {}({}), {}({})".format(n, int(nBrOdd), brOdd, int(nBrEv), brEv))
+  print("\n> заканчиваются в {}\n".format(convertTime(fullDur, bgn)))
+
+while 1:
+  string = input("введите кол-во уроков или -s:")
+  if string.lower() == '-s':
+    print("\n> --- программа остановлена ---\n")
+    break
+  calcTime(string)
