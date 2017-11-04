58652171d74208af196164027daa90665b3ebd1b
shmonoff <shmonoff@mail.ru>
2017-11-04 13:22:30 +1000

    Update 01-02.py

--- lesson-1/01-02.py
+++ lesson-1/01-02.py
@@ -1,13 +1,14 @@
-def countTables():
-  
+def countTables(n):
   def itemTables(x):
     if x%2: x+=1
     return x/2
-  
-  students = map(int, input("введите кол-во учеников 3-х классов через пробел:").split())
+  students = map(int, n.split())
   tables = int(sum(list(map(itemTables, students))))
+  print ("\n> требуется столов: {}\n".format(tables))
 
-  print ("требуется столов: {}".format(tables))
-
-
-countTables()
+while 1:
+  string = input("введите кол-во учеников 3-х классов через пробел или -s:")
+  if string.lower() == '-s':
+    print("\n> --- программа остановлена ---\n")
+    break
+  countTables(string)
