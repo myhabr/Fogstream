08d6e2b809d7fc4f5ac5589013d5f8c4711d5f50
shmonoff <shmonoff@mail.ru>
2017-11-04 13:07:29 +1000

    Update 01-03.py

--- lesson-1/01-03.py
+++ lesson-1/01-03.py
@@ -1,24 +1,19 @@
-def minToClock():
-  
+def minToClock(n):
+  n = int(n)
   h = 0; m = 0
-  
-  n = int(input("введите кол-во минут:"))
-  
   if n:
-    
     diff = n - n % (60*24)
-    
     if diff: n = n % (60*24)
-
     m = n % 60
     diff = n - m
-    
     if diff: h = int(diff/60)
+  if h < 10: h = "0"+str(h) # h = "{0:0>2}".format(h)
+  if m < 10: m = "0"+str(m) # m = "{0:0>2}".format(m)
+  print ("\n> {}:{}\n".format(h,m))
 
-  if h < 10: h = "0" + str(h)
-  if m < 10: m = "0" + str(m)
-
-  print ("на часах {}:{}".format(h,m))
-
-
-minToClock()
+while 1:
+  string = input("введите кол-во минут или -s: ")
+  if string.lower() == '-s':
+    print("\n> --- программа остановлена ---\n")
+    break
+  minToClock(string)
