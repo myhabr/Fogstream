63132a708d01900cc9c48f5acf1717d62775ba43
shmonoff <shmonoff@mail.ru>
2017-11-04 13:31:35 +1000

    Update 01-01.py

--- lesson-1/01-01.py
+++ lesson-1/01-01.py
@@ -2,30 +2,27 @@ import math
 import cmath
 
 
-def convertComplex():
-
-  x, y = map(float, input("введите X Y:").split())
-
+def convertComplex(n):
+  x, y = map(float, n.split())
   z = math.sqrt(x*x + y*y) # math.hypot(x, y)
-
   _phi = 0
   m = [0,0]
-
   if z:
-
     _phi = math.asin(math.fabs(y)/z)
     m = [0,1]
-
     if x < 0:       m = [ 1, 0]
     if y < 0:       m = [ 0,-1]
     if x<0 and y>0: m = [ 1,-1]
     if x<0 and y<0: m = [-1, 1]
     if x>0 and y<0: m = [ 0,-1]
-
   phi = m[0]*math.pi + m[1]*_phi
-
-  print("({}, {})".format(z, phi))
+  print("\n({}, {})".format(z, phi))
   print(cmath.polar(complex(x,y)))
+  print()
 
-
-convertComplex()
+while 1:
+  string = input("введите X Y через пробел или -s:")
+  if string.lower() == '-s':
+    print("\n> --- программа остановлена ---\n")
+    break
+  convertComplex(string)
