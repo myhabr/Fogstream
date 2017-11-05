178f9bb56682939b570b2bc2335b87e621ba027b
shmonoff <shmonoff@mail.ru>
2017-11-06 00:20:26 +1000

    Update 01-03.py

--- lesson-1/01-03.py
+++ lesson-1/01-03.py
@@ -1,14 +1,7 @@
 def minToClock(n):
-  n = int(n)
-  h = 0; m = 0
-  if n:
-    diff = n - n % (60*24)
-    if diff: n = n % (60*24)
-    m = n % 60
-    diff = n - m
-    if diff: h = int(diff/60)
-  if h < 10: h = "0"+str(h) # h = "{0:0>2}".format(h)
-  if m < 10: m = "0"+str(m) # m = "{0:0>2}".format(m)
+  n = n % (60*24)
+  h = "{0:0>2}".format(n//60)
+  m = "{0:0>2}".format(n%60)
   print ("\n> {}:{}\n".format(h,m))
 
 while 1:
@@ -16,4 +9,4 @@ while 1:
   if string.lower() == '-s':
     print("\n> --- программа остановлена ---\n")
     break
-  minToClock(string)
+  minToClock(int(string))
